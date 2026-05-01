d237185a7819e458df9f3306d89e91d92edf4905
TeraSurror <harshshelar22@gmail.com>
2026-05-01 14:31:08 -0400

    delete task

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod store;
 
 use crate::{
     cli::{Cli, Command},
-    store::{add_task, complete_task, list_tasks},
+    store::{add_task, complete_task, delete_task, list_tasks},
 };
 use clap::Parser;
 
@@ -20,5 +20,6 @@ fn main() -> anyhow::Result<()> {
         } => add_task(title, priority, due),
         Command::List { filter } => list_tasks(filter),
         Command::Complete { id } => complete_task(id),
+        Command::Delete { id } => delete_task(id),
     }
 }
